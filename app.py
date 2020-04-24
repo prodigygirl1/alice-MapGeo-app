41b21fbb673aacf6ebf221c6fea1ad32e0d74264
prodigygirl1 <Prodigygirl1.0@yandex.ru>
2020-04-24 20:00:53 +0400

    add acquaintance

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, filename='app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
+sessionStorage = {}
+
 
 @app.route('/post', methods=['POST'])
 def main():
@@ -35,28 +37,56 @@ def handle_dialog(res, req):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
-
-        res['response']['text'] = \
-            'Привет! Я могу сказать в какой стране город или сказать расстояние между городами!'
-
+        res['response']['text'] = 'Привет! Я могу сказать в какой стране город или сказать ' \
+                                  'расстояние между городами. Назови своё имя!'
+        sessionStorage[user_id] = {
+            'first_name': None,  # здесь будет храниться имя
+            'game_started': False
+        # здесь информация о том, что пользователь начал игру. По умолчанию False
+        }
         return
 
-    cities = get_cities(req)
-
-    if len(cities) == 0:
-
-        res['response']['text'] = 'Ты не написал название не одного города!'
-
-    elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - ' + get_geo_info(cities[0], 'country')
-    elif len(cities) == 2:
-
-        distance = get_distance(get_geo_info(cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
-        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
-
+    if sessionStorage[user_id]['first_name'] is None:
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            # создаём пустой массив, в который будем записывать города, которые пользователь уже отгадал
+            sessionStorage[user_id]['guessed_cities'] = []
+            # как видно из предыдущего навыка, сюда мы попали, потому что пользователь написал своем имя.
+            # Предлагаем ему сыграть и два варианта ответа "Да" и "Нет".
+            res['response'][
+                'text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Отгадаешь город по фото?'
+            res['response']['buttons'] = [
+                {
+                    'title': 'Да',
+                    'hide': True
+                },
+                {
+                    'title': 'Нет',
+                    'hide': True
+                }
+            ]
     else:
+        cities = get_cities(req)
 
-        res['response']['text'] = 'Слишком много городов!'
+        if len(cities) == 0:
+
+            res['response']['text'] = 'Ты не написал название не одного города!'
+
+        elif len(cities) == 1:
+            res['response']['text'] = 'Этот город в стране - ' + get_geo_info(cities[0], 'country')
+        elif len(cities) == 2:
+
+            distance = get_distance(get_geo_info(cities[0], 'coordinates'),
+                                    get_geo_info(cities[1], 'coordinates'))
+            res['response']['text'] = 'Расстояние между этими городами: ' + str(
+                round(distance)) + ' км.'
+
+        else:
+
+            res['response']['text'] = 'Слишком много городов!'
 
 
 def get_cities(req):
@@ -71,6 +101,16 @@ def get_cities(req):
                 cities.append(entity['value']['city'])
 
     return cities
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name', то возвращаем её значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
